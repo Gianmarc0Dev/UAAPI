15043d53bf5f311e28b54a4b66f3c13b8a283d3c
Gianmarco <72602391+Gianmarc0Dev@users.noreply.github.com>
2021-09-06 18:28:24 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings
+    name='UAAPI',
+    url='https://github.com/Gianmarc0Dev/UAAPI',
+    author='Gianmarco',
+    author_email='worldwideoutlander@gmai.com',
+    # Needed to actually package something
+    packages=['API'],
+    # Needed for dependencies
+    install_requires=[''],
+    # *strongly* suggested for sharing
+    version='1.0',
+    license='MIT',
+    description='An unoffical amazon API',
+    # We will also need a readme eventually (there will be a warning)
+    long_description=open('README.txt').read(),
+    # if there are any scripts
+    #scripts=['scripts/hello.py'],
+)
